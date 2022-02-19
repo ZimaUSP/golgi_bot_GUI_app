2ab120275f55159ccb22768b488b5b60bcf1ae17
PedroCroso <pedroroso@usp.br>
2022-02-19 20:39:58 -0300

    ⚡️ limited items per screen

--- Golgi_v0.1/delete_screen.py
+++ Golgi_v0.1/delete_screen.py
@@ -6,12 +6,12 @@
     Author:
         Pedro Croso <pedrocroso@usp.br>
 """
+from math import ceil
 from kivymd.uix.boxlayout import MDBoxLayout
 from edit_screen import ResultadoBinario
 
 from dataframe import *
 
-import os
 
 class DeletePage(MDBoxLayout):
     """Page to delete item
@@ -22,27 +22,63 @@ class DeletePage(MDBoxLayout):
     def __init__(self, **kwargs):
         """Initialisation method
         """
+        self.current_page = 0
         super().__init__(**kwargs)
     
     def on_submit(self):
         """Callback function to search for results
         """
-        data = golgi_data.get_items(id = self.ids.busca.ids.id.text, nome = self.ids.busca.ids.nome.text, dosagem=self.ids.busca.ids.dose.text, apresentacao=self.ids.busca.ids.apresentacao.text)
-        self.ids.resultados.ids.main_layout.clear_widgets()
+        self.data = golgi_data.get_items(id = self.ids.busca.ids.id.text, nome = self.ids.busca.ids.nome.text, dosagem=self.ids.busca.ids.dose.text, apresentacao=self.ids.busca.ids.apresentacao.text)
+        #self.ids.resultados.ids.main_layout_resultados.clear_widgets()
         self.ids.busca.ids.nome.text = ""
         self.ids.busca.ids.id.text = ""
         self.ids.busca.ids.dose.text = ""
         self.ids.busca.ids.apresentacao.text = ""
-        data = data.reset_index()
-        for index in data.index:
-            new_item = ResultadoBinario()
-            new_item.set_nome(str(data['nome'][index]))
-            new_item.set_id(str(data['id'][index]))
-            new_item.set_dose(str(data['dosagem'][index]))
-            new_item.set_apresentacao(str(data['apresentacao'][index]))
-            new_item.set_position(str(data['position'][index]))
-            new_item.generate()
-            self.ids.resultados.ids.main_layout.add_widget(new_item)
+        self.data = self.data.reset_index()
+        self.n_items  = len(self.data.index)
+        self.n_pages = ceil(self.n_items/15.0)
+        self.current_page = 0
+        self.ids.current_page.text = "1"
+        self.ids.go_to_last.text = str(self.n_pages)
+        self.load_items()
+        
+        print(self.n_items)
+    
+    def load_items(self):
+        self.ids.main_layout_resultados.clear_widgets()
+        self.ids.current_page.text = str(self.current_page + 1)
+        for index in range(15): #self.data.index:
+            index = 15*self.current_page + index
+            if index < self.n_items:
+                new_item = ResultadoBinario()
+                new_item.set_nome(str(self.data['nome'][index]))
+                new_item.set_id(str(self.data['id'][index]))
+                new_item.set_dose(str(self.data['dosagem'][index]))
+                new_item.set_apresentacao(str(self.data['apresentacao'][index]))
+                new_item.set_position(str(self.data['position'][index]))
+                new_item.generate()
+                #self.ids.resultados.ids.main_layout_resultados.add_widget(new_item)
+                self.ids.main_layout_resultados.add_widget(new_item)
+    
+    def on_go_to_first(self):
+        self.current_page = 0
+        self.load_items()
+    
+    def on_go_to_previous(self):
+        if self.current_page > 0:
+            self.current_page -= 1
+            self.load_items()
+    
+    def on_go_to_next(self):
+        if self.current_page < self.n_pages - 1:
+            self.current_page += 1
+            self.load_items()
+    
+    def on_go_to_last(self):
+        self.current_page = self.n_pages - 1
+        self.load_items()
+    
+
         
     def delete_release(self):
         """Callback function to delete the selected items
--- Golgi_v0.1/edit_screen.py
+++ Golgi_v0.1/edit_screen.py
@@ -6,6 +6,7 @@
     Author:
         Pedro Croso <pedrocroso@usp.br>
 """
+from math import ceil
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.gridlayout import MDGridLayout
 #from collect_screen import Resultado
@@ -27,6 +28,8 @@ class SearchEditPage(MDBoxLayout):
         self.dose_item = ""
         self.apresentacao_item = ""
         self.pos_item = ""
+        self.current_page = 0
+        
         super().__init__(**kwargs)
 
     
@@ -60,25 +63,57 @@ class SearchEditPage(MDBoxLayout):
     def on_submit(self):
         """Callback function to search for items to edit
         """
-        data = golgi_data.get_items(id = self.ids.busca.ids.id.text, nome = self.ids.busca.ids.nome.text, dosagem=self.ids.busca.ids.dose.text, apresentacao=self.ids.busca.ids.apresentacao.text)
-        self.ids.resultados.ids.main_layout.clear_widgets()
+
+        self.data = golgi_data.get_items(id = self.ids.busca.ids.id.text, nome = self.ids.busca.ids.nome.text, dosagem=self.ids.busca.ids.dose.text, apresentacao=self.ids.busca.ids.apresentacao.text)
+        #self.ids.resultados.ids.main_layout_resultados.clear_widgets()
         self.ids.busca.ids.nome.text = ""
         self.ids.busca.ids.id.text = ""
         self.ids.busca.ids.dose.text = ""
         self.ids.busca.ids.apresentacao.text = ""
-        print(data)
+        self.data = self.data.reset_index()
+        self.n_items  = len(self.data.index)
+        self.n_pages = ceil(self.n_items/15.0)
+        self.current_page = 0
+        self.ids.current_page.text = "1"
+        self.ids.go_to_last.text = str(self.n_pages)
+        self.load_items()
+        
+        print(self.n_items)
 
-        for index in data.index:
-            print(index)
-            new_item = ResultadoBinario()
-            new_item.set_nome(str(data['nome'][index]))
-            new_item.set_id(str(index))
-            new_item.set_dose(str(data['dosagem'][index]))
-            new_item.set_apresentacao(str(data['apresentacao'][index]))
-            new_item.set_position(str(data['position'][index]))
-            new_item.generate()
-            self.ids.resultados.ids.main_layout.add_widget(new_item)
-
+    def load_items(self):
+        self.ids.main_layout_resultados.clear_widgets()
+        self.ids.current_page.text = str(self.current_page + 1)
+        for index in range(15): #self.data.index:
+            index = 15*self.current_page + index
+            if index < self.n_items:
+                new_item = ResultadoBinario()
+                new_item.set_nome(str(self.data['nome'][index]))
+                new_item.set_id(str(self.data['id'][index]))
+                new_item.set_dose(str(self.data['dosagem'][index]))
+                new_item.set_apresentacao(str(self.data['apresentacao'][index]))
+                new_item.set_position(str(self.data['position'][index]))
+                new_item.generate()
+                #self.ids.resultados.ids.main_layout_resultados.add_widget(new_item)
+                self.ids.main_layout_resultados.add_widget(new_item)
+    
+    def on_go_to_first(self):
+        self.current_page = 0
+        self.load_items()
+    
+    def on_go_to_previous(self):
+        if self.current_page > 0:
+            self.current_page -= 1
+            self.load_items()
+    
+    def on_go_to_next(self):
+        if self.current_page < self.n_pages - 1:
+            self.current_page += 1
+            self.load_items()
+    
+    def on_go_to_last(self):
+        self.current_page = self.n_pages - 1
+        self.load_items()
+    
 
 
 class EditPage(MDGridLayout):
