37240f1cbb276f0efda5f38e5ca405aedd47c1de
PedroCroso <pedrocroso@usp.br>
2022-05-02 18:50:54 -0300

    ➕ add users is possible

--- Golgi_v0.1/log_in_screen.py
+++ Golgi_v0.1/log_in_screen.py
@@ -1,5 +1,6 @@
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.app import MDApp
+from data_users import *
 
 class LogInPage(MDBoxLayout):
     """Log In page declaration
@@ -13,7 +14,17 @@ class ActualLogIn(MDBoxLayout):
     def on_login(self):
         app= MDApp.get_running_app()
         
+        checker = golgi_users.validate(self.ids.user.text, self.ids.password.text)
+        adm = golgi_users.get_admin(self.ids.user.text)
+        
+        if checker:
+            if adm:
+                app.root.current = "admin_main_screen"
+            else:
+                app.root.current = "main_screen"
+        '''
         if self.ids.user.text == 'admin':
             app.root.current = "admin_main_screen"
         else:
             app.root.current = "main_screen"
+        '''
